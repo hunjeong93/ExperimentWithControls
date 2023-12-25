5f6c1a39474f08c48a6041a23773e065a456da61
hunjeong <rhkrgnswjd@naver.com>
2023-12-26 05:57:18 +0900

    컨트롤을 이용한 숫자 텍스트 입력

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -30,5 +30,47 @@ namespace ExperimentWithControls
         {
             e.Handled = !int.TryParse(e.Text, out int result);
         }
+
+        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = smallSlider.Value.ToString("0"); // Slider 컨트롤의 값은 소수점을 가진 실수이다. 0은 실수를 정수로 변환한다.
+        }
+
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = bigSlider.Value.ToString("000-0000-0000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if(sender is  RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(myListBox.SelectedItems is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
+        }
     }
 }
